efd7db87b9beebfdda07619f294a1be1f7d012e1
chamano sidney <chamano.sr@gmail.com>
2021-01-10 17:09:38 +0200

    create comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react'
 
+const Sample_Flashcards = [
+  {
+    id:1,
+    question: 'what is 2 + 2?',
+    answer:'4',
+    options:[
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  }
+]
 
 function App() {
   return (
     <h1>Hello chamano</h1>
   );
 }
+
 
 export default App;
