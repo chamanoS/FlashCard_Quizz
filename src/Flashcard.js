6619c2b549083373781370a814cb8098d7b0ae17
chamano sidney <chamano.sr@gmail.com>
2021-01-11 16:51:43 +0200

    display cont

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -4,8 +4,17 @@ export default function Flashcard({flashcard}) {
     const [flip, setFlip] = useState(false)
 
     return (
-        <div onClick={() => setFlip(!flip)}>
-            {flip ? flashcard.answer : flashcard.question}
+        <div className={`card ${flip ? 'flip' : ''}`}
+         onClick={() => setFlip(!flip)}>
+             <div className="front">
+                 {flashcard.question}
+                 <div className="flashcard-options">
+                    {flashcard.options.map(option => {
+                        return <div className="flashcard-options">{option}</div>
+                    })}
+                 </div>
+             </div>
+            <div className="back"> {flashcard.answer}</div>
         </div>
     )
 }
